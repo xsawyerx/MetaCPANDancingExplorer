520d697c26366d2040d9205325207c29ccfe23d5
Sawyer X <xsawyerx@cpan.org>
2011-08-08 00:10:51 +0300

    create all routes in the main page

--- lib/MetaCPANDancingExplorer.pm
+++ lib/MetaCPANDancingExplorer.pm
@@ -1,22 +1,44 @@
 package MetaCPANDancingExplorer;
 use Dancer ':syntax';
+use Capture::Tiny 'capture';
 use WWW::GitHub::Gist;
 
 our $VERSION = '0.1';
 
-var gist => WWW::GitHub::Gist->new( id => 1119648 );
+# basic variables
+my $gist  = WWW::GitHub::Gist->new( id => 1119648 );
+my @pages = qw/author/;
 
+# setting up routes for all pages
+foreach my $page (@pages) {
+    get "/$page" => sub {
+        template "${page}s";
+    };
+
+    get "/$page/:file" => sub {
+        my $file    = params->{'file'};
+        my $content = $gist->file($file);
+        $content =~ s/\n//g;
+        $content .= <<"____END";
+use Data::Dump 'dd';
+dd \$$page;
+____END
+
+        return capture { eval $content } || 'error';
+    };
+}
+
+# setting up variables for all templates
 before_template sub {
     my $tokens = shift;
 
     $tokens->{'gistp'} = 'https://gist.github.com/1119648.js?file=';
 };
 
+# main route
 get '/' => sub {
     template 'index';
 };
-
-load 'MetaCPANDancingExplorer/Author.pm';
 
 true;
 
